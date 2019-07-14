b90307a5e6fdb226abcba808c271c9d78dba691b
oakes <zach@sekao.net>
2019-07-14 12:49:14 -0400

    Step 0

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
-int main()
-{
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
+std::string READ(const std::string input) {
+    return input;
+}
+
+std::string EVAL(const std::string input) {
+    return input;
+}
+
+std::string PRINT(const std::string input) {
+    return input;
+}
+
+std::string rep(const std::string input) {
+    return PRINT(EVAL(READ(input)));
+}
+
+int main(int argc, char* argv[]) {
+    std::string input;
+    do {
+        std::cout << "user> ";
+        std::getline(std::cin, input);
+        std::cout << rep(input) << std::endl;
+    } while (!std::cin.fail());
+    return 0;
 }
