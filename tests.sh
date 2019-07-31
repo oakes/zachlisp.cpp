f23b875827e05e695eded2736a4d26160b8a943f
oakes <zach@sekao.net>
2019-07-31 09:35:05 -0400

    Update test script

--- tests.sh
+++ tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 EXEC=zachlisp
-STEP=${1:-step1_read_print}
-g++ repl.cpp -o $EXEC -std=c++17 && ./tests/runtest.py tests/$STEP.mal -- ./$EXEC
+STEP=${1:-step2_eval}
+./tests/runtest.py tests/$STEP.mal -- ./$EXEC
